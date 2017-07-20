9ec60b2a5bd604430717cfe84d6e44e4e880f2c2
Daniel Scott <dnscott87@gmail.com>
2017-07-20 12:41:19 -0400

    WIP commit, fitAG and friends to yield weights

--- src/AGfunctions2.R
+++ src/AGfunctions2.R
@@ -1,70 +1,69 @@
-## This file needs to be sourced in R
-## functions to fit ancestral graphs from fmri data
-## tailored to Sara data
-## first you need to decide which conditions to test en which to aggregate
-
-# check whether package "ggm" is available. If not, install and obtain
-if(!("ggm" %in% .packages(all=TRUE))) install.packages(ggm)
+## Functions to fit ancestral graphs from fmri data
 require(ggm)
 
 # make object "brain"
-as.brain <- function(yData,rois=NA,cond=NA,...)
-{
-  if(!is.na(rois[1])) yData <- yData[,rois]
-  if(!is.na(cond[1])) yData <-yData[cond,]
-  attr(yData,"n") <- length(cond)
-  class(yData) <- c("brain","data.frame")
-  return(yData)
+as.brain <- function(acts, rois = NA, cond = NA, ...) {
+  if(!is.na(rois[1])) acts <- acts[,rois]
+  if(!is.na(cond[1])) acts <-acts[cond,]
+  attr(acts,"n") <- length(cond)
+  class(acts) <- c("brain","data.frame")
+  return(acts)
 }
 
 
-aic.dev <- function(fit,S,n)
-{
+getAIC <- function(fit, S, n) {
   p <- length(fit$Shat[1,])
-  q <- p*(p-1)/2 - fit$df
-  npars <- p+q
-  fitS <- -(n/2)*log(2*pi) -(n/2)*log(det(S)) -(n*p)/2
-  aic <- fit$dev + fitS + 2*npars
-  return(list(aic=aic,npars=npars))
+  q <- p*(p - 1) /2 - fit$df
+  npars <- p + q
+  fitS  <- -(n/2)*log(2*pi) -(n/2)*log(det(S)) -(n*p)/2
+  aic   <- fit$dev + fitS + 2*npars
+  return(list(aic = aic, npars = npars))
 }
 
 
+fitAG <- function(acts, adjacency, rois = NA, cond = NA, detail = "AIC", ...) {
+  # Fits all subjects' data using an AG model
+  #
+  # Args:
+  #   acts     : Array-like activation data. Trials as rows and rois as columns.
+  #   adjacency: Adjacency matrix
+  #   rois     : Regions of interest
+  #   cond     : conditions
+  #   detail   : Level of detail required: 'AIC', 'LR', or 'both'
+  #
+  # Returns:
+  #   
 
-
-
-# function to fit all subjects using an AG model
-# result is aic value for each subject on the model
-# or if <detail="LR"> then a likelihood ratio test is performed
-# or if <detail="both"> then both results are given
-fitAG <- function(yData,amat,rois=NA,cond=NA,detail="AIC",...)
-{
-  N <- length(yData)
+  N <- length(acts)
   n <- length(cond)
   p <- length(rois)
   results <- numeric(0)
-  for(i in 1:N)
-  {
-    Data <- as.brain(yData[[i]],rois,cond)
-    S <- makeCov(Data)
-    fitS <- fitAncestralGraph(amat,S,attr(S,"n"))
-    if(detail=="AIC") results <- c(results,aic=aic.dev(fitS,S,n)$aic)
-    if(detail=="LR")  results <- c(results, list(chi2=T2(fitS$dev,n),p=pchisq(T2(fitS$dev,n),fitS$df,lower.tail=FALSE)))
-    if(detail=="both") results <- c(results,list(fit=list(chi2=T2(fitS$dev,n),p=pchisq(T2(fitS$dev,n),fitS$df,lower.tail=FALSE))),aic=aic.dev(fitS,S,n)$aic,npars=aic.dev(fitS,S,n)$npars)
+  
+  for(i in 1:N) {
+
+    data  <- as.brain(acts[[i]], rois, cond)
+    covar <- makeCov(data)
+    
+    covar.fit <- fitAncestralGraph(adjacency, covar, attr(covar,"n"))
+
+    chi2  <- T2(s.fit$dev, n)
+    p     <- pchisq(chi2, s.fit$df, lower.tail = FALSE)    
+    fit   <- list(chi2, p)
+
+    aic   <- getAIC(s.fit, s, n)
+  
+    results <- c(results, fit, aic$aic, aic$npars)
+
   }
   return(results)
 }
 
-makeCov <- function(yData)
-{
-    S <- cov(yData)
-    attr(S,"n") <- attr(yData,"n")
+makeCov <- function(acts) {
+    S <- cov(acts)
+    attr(S,"n") <- attr(acts,"n")
     return(S)
 }
 
-
-T2 <- function(fit,n)
-{
+T2 <- function(fit, n) {
     fit/(1+(fit/n))
 }
-
-
